62a1068c50b6d10678e4c53fead4b80fa1f6ab87
Michał Mika <michal@mika.sh>
2025-06-15 14:19:08 +0200

    migration from gitlab

---
+++ test/runtests.jl
@@ -0,0 +1,20 @@
+using Test
+
+tests = [
+            "base",
+            "kroneckerproduct",
+            "kroneckersum",
+            "kronprops",
+            "factorizations",
+            "contract",
+            "boxproduct",
+            "boxprops"
+        ]
+
+@testset "KroneckerProducts" begin
+    for t in tests
+        fp = joinpath(dirname(@__FILE__), "$t.jl")
+        println("$fp ...")
+        include(fp)
+    end
+end # @testset
